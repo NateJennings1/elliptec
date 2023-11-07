e14ae03f0f105166ac5036bf98eee3cc3b1835f2
Nate Jennings <njenn1.618@gmail.com>
2023-11-07 10:08:12 -0800

    Customized linear stage commands for my particular device. Added velocity commands and enforce a duty cycle according to the ell17 linear stage specs.

--- src/elliptec/linear.py
+++ src/elliptec/linear.py
@@ -1,39 +1,47 @@
-"""Module for linear stages. Inherits from elliptec.Motor."""
+from .devices import devices
 from . import Motor
+import time
+
 
 
 class Linear(Motor):
-    """Elliptec Linear Motor class. Inherits from elliptec.Motor."""
-
-    def __init__(self, controller, address="0", debug=True):
+    
+    def __init__(self, controller, address='0', debug=True, inverted=False):
         # Patch parent object - elliptec.Motor(port, baud, bytesize, parity)
         super().__init__(controller=controller, address=address, debug=debug)
-
+        
+    
     ## Setting and getting positions
     def get_distance(self):
-        """Finds at which distance the stage is at the moment."""
-        status = self.get("position")
+        ''' Finds at which distance the stage is at the moment. '''
+        status = self.get('position')
         distance = self.extract_distance_from_status(status)
         return distance
-
+    
     def set_distance(self, distance):
-        """Moves to a particular distance."""
+        ''' Moves to a particular distance. '''
+        distance = distance+0.0047
         position = self.dist_to_pos(distance)
-        status = self.move("absolute", (position))
+        status,t1 = self.move('absolute', (position))
         distance = self.extract_distance_from_status(status)
-        return distance
+        return distance,t1
 
     def shift_distance(self, distance):
-        """Shifts by a particular distance."""
+        if distance < 0 and abs(distance)<0.03:
+            distance = (distance - 0.002)
+        elif distance < 0:
+            distance = (distance - 0.002)
+        elif distance > 0 and abs(distance)<1:
+            distance = (distance + 0.005)
+
         position = self.dist_to_pos(distance)
-        status = self.move("relative", position)
+        status,t1 = self.move('relative', position)
         distance = self.extract_distance_from_status(status)
-        return distance
+        return distance,t1
 
     def jog(self, direction="forward"):
-        """Jogs by the jog distance in a particular direction."""
         if direction in ["backward", "forward"]:
-            status = self.move(direction)
+            status,t1 = self.move(direction)
             position = self.extract_distance_from_status(status)
             return position
         else:
@@ -41,54 +49,59 @@ class Linear(Motor):
 
     # Home set/get
     def get_home_offset(self):
-        """Gets the home offset."""
-        status = self.get("home_offset")
+        status = self.get('home_offset')
         distance = self.extract_distance_from_status(status)
         return distance
 
     def set_home_offset(self, offset):
-        """Sets the home offset."""
         position = self.dist_to_pos(offset)
-        status = self.set("home_offset", position)
+        status = self.set('home_offset', position)
         return status
 
     # Jog step
     def get_jog_step(self):
-        """Gets the jog step."""
-        status = self.get("stepsize")
+        status = self.get('stepsize')
         distance = self.extract_distance_from_status(status)
         return distance
 
     def set_jog_step(self, distance):
-        """Sets jog step to a particular distance (in millimeters)."""
+        ''' Sets jog step to a particular distance (in millimeters). '''
         position = self.dist_to_pos(distance)
-        status = self.set("stepsize", position)
+        status = self.set('stepsize', position)
         return status
 
+    def set_velocity(self,velocity):
+        velocity *= 16**6
+        status = self.set('velocity',velocity)
+        return status
+    
+    def get_velocity(self):
+        status = int(self.get('velocity')[2],16)
+        return status
+    
     # TODO: clean(self)
     # TODO: clean_and_optimize(self)
 
     # Helper functions
     def extract_distance_from_status(self, status):
-        """Extracts distance from status."""
         # If status is telling us current position
         if status:
-            if status[1] in ["PO", "HO", "GJ"]:
+            if status[1] in ['PO', 'HO', 'GJ']:
                 position = status[2]
                 distance = self.pos_to_dist(position)
                 return distance
-
+       
         return None
 
     def pos_to_dist(self, position):
-        """Converts position in pulses to distance in millimeters."""
+        ''' Converts position in pulses to distance in millimeters. '''
         pulse_range = self.pulse_per_rev * self.range
-        distance = position / pulse_range * self.range
+        distance = position/pulse_range * self.range
         distance_rounded = round(distance, 4)
         return distance_rounded
-
+        
     def dist_to_pos(self, distance):
-        """Converts distance in millimeters to position in pulses."""
+        ''' Converts distance in millimeters to position in pulses. '''
         pulse_range = self.pulse_per_rev * self.range
-        position = int(distance / self.range * pulse_range)
+        position = int(distance/self.range * pulse_range)
         return position
